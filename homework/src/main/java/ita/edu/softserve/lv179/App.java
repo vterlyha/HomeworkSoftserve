8a9c6c0741981e1387921601be8b005ce966b4e3
Volodymyr95 <krokhmalyuk@live.com>
2016-03-14 19:37:56 +0200

    create class Menu and added menu functional

--- homework/src/main/java/ita/edu/softserve/lv179/App.java
+++ homework/src/main/java/ita/edu/softserve/lv179/App.java
@@ -1,5 +1,7 @@
 package ita.edu.softserve.lv179;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       
+        
+        Menu menu = new Menu();
+        menu.caseMenuItem();
     }
 }
