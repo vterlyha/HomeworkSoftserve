c216cc65db9c0f11c3c2b9b967d1200bbe2a373a
vterlyha <vterlyha@gmail.com>
2016-03-16 00:05:06 +0200

    added my tests

--- src/main/java/ita/edu/softserve/lv179/usersclases/HomeworkTerlyha.java
+++ src/main/java/ita/edu/softserve/lv179/usersclases/HomeworkTerlyha.java
@@ -3,6 +3,8 @@
  */
 package ita.edu.softserve.lv179.usersclases;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -21,32 +23,60 @@ public final class HomeworkTerlyha {
     }
 
     /**
-     * Method finds the smallest number of format 2^r that is bigger than given natural number.
-     *
-     *  Parameters (naturalNumber) is entered from Command Line.
+     * Method solves task 108.
      */
     public static void solveExcercise108() {
         System.out.println("Find the smallest number of format 2^r that is bigger than given natural number");
+        @SuppressWarnings("resource")
         Scanner sc = new Scanner(System.in);
-        int naturalNumber = sc.nextInt();
-        System.out.println("Here is your number");
+        System.out.println("Here is your number: "
+        + HomeworkTerlyha.findNumberForTask108(sc.nextInt()));
+    }
+
+    /**
+     * Method solves task 325.
+     */
+    public static void solveExcercise325() {
+        System.out.println("Find simple dividers of given natural number");
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Here are your dividers: "
+                + HomeworkTerlyha.findDividersForTask325(sc.nextInt()));
+    }
+
+    /**
+     * Method solves task 325.
+     */
+    public static void solveExcercise561() {
+        System.out.println("Find all natural numbers, that coincide with last numbers of its squares");
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Here are your numbers: "
+                + HomeworkTerlyha.findNaturalNumbersForTask561(sc.nextInt()));
+    }
+
+    /**
+     * Method used in solveExcercise108().
+     * 
+     * @param naturalNumber natural number that is entered from console.
+     * @return returns all natural numbers, that coincide with last numbers of its squares.
+     */
+    public static int findNumberForTask108(int naturalNumber) {
         int result = 1;
         while (result <= naturalNumber) {
             result *= 2;
         }
-        System.out.println(result);
+        return result;
     }
 
     /**
-     * Method finds simple dividers of given natural number.
-     *
-     *  Parameters (naturalNumber) is entered from Command Line.
+     * Method used in solveExcercise325().
+     * 
+     * @param naturalNumber natural number that is entered from console.
+     * @return returns simple dividers of given natural number.
      */
-    public static void solveExcercise325() {
-        System.out.println("Find simple dividers of given natural number");
-        Scanner sc = new Scanner(System.in);
-        int naturalNumber = sc.nextInt();
-        System.out.println("Here are your dividers");
+    public static List<Integer> findDividersForTask325(int naturalNumber) {
+        List<Integer> simpleDividers = new ArrayList<Integer>();
         for (int i = 2; i < naturalNumber; i++) {
             if ((naturalNumber % i) == 0) {
                 int divider = 0;
@@ -56,37 +86,35 @@ public final class HomeworkTerlyha {
                     }
                 }
                 if (divider == 0) {
-                    System.out.print(i + " ");
+                    simpleDividers.add(i);
                 }
             }
         }
-        System.out.println();
+        return simpleDividers;
     }
 
     /**
-     * Method finds all natural numbers, that coincide with last numbers of its squares.
-     *
-     *  Parameters (naturalNumber) is entered from Command Line.
+     * Method used in solveExcercise561().
+     * 
+     * @param naturalNumber natural number that is entered from console.
+     * @return returns all natural numbers, that coincide with last numbers of its squares.
      */
-    public static void solveExcercise561() {
-        System.out.println("Find all natural numbers, that coincide with last numbers of its squares");
+    public static List<Integer> findNaturalNumbersForTask561(int naturalNumber) {
         final int modulusDividerIncrement = 10;
-        Scanner sc = new Scanner(System.in);
-        int naturalNumber = sc.nextInt();
-        System.out.println("Here are your numbers");
+        List<Integer> naturalNumbers = new ArrayList<Integer>();
         for (int i = 1; i <= naturalNumber; i++) {
             int length = Integer.toString(i).length();
             int modulusDivider = modulusDividerIncrement;
             for (int a = 0; a < length; a++) {
                 int result = (i * i) % modulusDivider;
                 if (result == i) {
-                    System.out.print(result + " ");
+                    naturalNumbers.add(result);
                     break;
                 }
                 modulusDivider *= modulusDividerIncrement;
             }
         }
-        System.out.println();
+        return naturalNumbers;
     }
 }
 
