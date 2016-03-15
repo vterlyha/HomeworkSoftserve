d78e06f25bc75a27999e672f835b5b832fbbc065
andyislegend <andrew.petryk@gmail.com>
2016-03-15 10:17:34 +0200

    several changes where made: 1) added my class for homework task 2) added @javadoc comments 3) re-build switch 4) Fix bug in Menu menuitem[] folder

--- src/main/java/ita/edu/softserve/lv179/App.java
+++ src/main/java/ita/edu/softserve/lv179/App.java
@@ -1,17 +1,21 @@
 package ita.edu.softserve.lv179;
 
-import java.util.Scanner;
-
 /**
- * Hello world!
- *
+ * Class App perform the execution of application.
+ * 
+ * @author lv179jv
+ * @version 2.0.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-              
-        Menu menu = new Menu();
-        menu.caseMenuItem();
-    }
+public class App {
+
+	/**
+	 * Method main is used as a entering point into program execution.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Menu menu = new Menu();
+		menu.caseMenuItem();
+	}
 }
--- src/main/java/ita/edu/softserve/lv179/usersclases/HomeworkVarvariuk.java
+++ src/main/java/ita/edu/softserve/lv179/usersclases/HomeworkVarvariuk.java
@@ -1,43 +1,51 @@
-package edu.ita.Lv179jv;
+package ita.edu.softserve.lv179.usersclases;
+
 import java.util.Scanner;
 
 public class HomeworkVarvariuk {
-	public static void solveExercise86a(){
+
+	public static void solveExercise86a() {
 		System.out.println("Write down positive integer; how many digits does it contain?:");
+
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		int number = num;
 		Integer counter = 0;
-		while ((number%10) != 0){
-			number = number/10;
+		while ((number % 10) != 0) {
+			number = number / 10;
 			counter++;
 		}
 		System.out.println("The counter is: " + counter);
 	}
-	
-	public static void solveExercise86b(){
+
+	public static void solveExercise86b() {
 		System.out.println("Write down positive integer; What is the sum of it's digits?:");
+
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		int number = num;
-		Integer sum = number%10;
-		while ((number%10) != 0){
-			number = number/10;
-			sum += number%10;
+		Integer sum = number % 10;
+		while ((number % 10) != 0) {
+			number = number / 10;
+			sum += number % 10;
 		}
 		System.out.println("The sum is: " + sum);
 	}
-	
-	public static void solveExercise330(){
+
+	public static void solveExercise330() {
 		System.out.println("Positive integer is perfect if it is equal to the sum of all it's dividers, exept itself:");
 		System.out.println("Enter the number to find all perfect numbers that are less than it");
+
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		String output = "Perfect sums: ";
 		int perfectSum = 0;
 		for (int i = 1; i < num; i++) {
 			for (int j = 1; j < i; j++) {
-				if ((i%j) == 0) {
+				if ((i % j) == 0) {
 					perfectSum += j;
 				}
 			}
@@ -46,6 +54,7 @@ public class HomeworkVarvariuk {
 			}
 			perfectSum = 0;
 		}
-		System.out.println(output);;
+		System.out.println(output);
+		;
 	}
 }
